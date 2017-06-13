1acd7f05995bb52b1bcfa69c286c0c676a68f1b7
Ubuntu <shinahara@human.ait.kyushu-u.ac.jp>
2017-06-13 19:17:50 +0900

    色々と変更

--- TrainVggModel.py
+++ TrainVggModel.py
@@ -85,7 +85,7 @@ def buildCaffenet(class_num, channel_num=3, img_width=224, img_height=224):
   model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
   model.add(BatchNormalization())
   # Conv2
-  model.add(Convolution2D(256, 5, 5, border_mode='same', W_regularizer=l2(weight_decay), 
+  model.add(Convolution2D(256, 4, 4, border_mode='same', W_regularizer=l2(weight_decay), 
                           name='conv2'))
   model.add(Activation('relu'))
   model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
@@ -122,15 +122,12 @@ def buildCaffenet(class_num, channel_num=3, img_width=224, img_height=224):
 
 def main():
   class_num = 2
-  parent_dirpath = '/media/snhryt/Data/Research_Master/keras/MyWork/2fonts_2class/'
-  weights_filepath = parent_dirpath + 'weights.hdf5'
-  json_filepath = parent_dirpath + 'model.json'
+  parent_dirpath = '/media/snhryt/Data/Research_Master/keras/MyWork/2fonts_2class_exceptCOQaceot/'
+  model_filepath = parent_dirpath + 'model.hdf5'
 
-  if (os.path.exists(weights_filepath) and os.path.exists(json_filepath)):
-    print('\n[Loading the model and its weights]')
-    with open(json_filepath) as f:
-      model = model_from_json(f.read())
-    model.load_weights(weights_filepath)
+  if (os.path.exists(model_filepath)):
+    print('\n[Loading and the model and its weights]')
+    model = load_model(model_filepath)
   else:
     # 画像とラベルの読み込み
     train_txt_filepath = parent_dirpath + 'train.txt'
@@ -146,27 +143,17 @@ def main():
 
     # callbacksの設定
     tbcb = TensorBoard(log_dir=parent_dirpath+'graph', histogram_freq=0, write_graph=True)
-    checkpointer = ModelCheckpoint(weights_filepath, monitor='val_loss', verbose=1, 
-                                   save_best_only=True)
+    checkpointer = ModelCheckpoint(model_filepath, monitor='val_loss', verbose=1, 
+                                   save_best_only=True, save_weights_only=False)
     # patientce： 何回連続で損失の最小値が更新されなかったらループを止めるか
     early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
     
     # モデルの学習
     batch_size = 256
-    epoch_num = 100
+    epoch_num = 50
     history = model.fit(train_img_arrays, train_labels, batch_size=batch_size, epochs=epoch_num,
                         verbose=1, validation_data=(val_img_arrays, val_labels), 
-                        callbacks=[tbcb, early_stopping, checkpointer])
-    
-    # モデル構造を.json&.png形式で保存
-    png_filepath = parent_dirpath + 'model.png'
-    model_json = model.to_json()
-    with open(json_filepath, mode='w') as f:
-      f.write(model_json)
-    plot_model(model, to_file=png_filepath)
-
-    # 学習済の重みの保存
-    model.save_weights(weights_filepath)
+                        callbacks=[tbcb, checkpointer, early_stopping])
     
     # 学習履歴の保存
     history_filepath = parent_dirpath + 'history.pickle'
@@ -179,7 +166,6 @@ def main():
     history = None
     with open(history_filepath, mode='rb') as f:
       history = pickle.load(f)
-
     # y軸の範囲限定版
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(2, 1, 1)
@@ -208,6 +194,24 @@ def main():
     ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.3), shadow=True, ncol=2)
     fig2.savefig(img_filepath2)
     plt.close()
+
+    # モデル構造の書き出し
+    png_filepath = parent_dirpath + 'model.png'
+    plot_model(model, to_file=png_filepath, show_shapes=True, show_layer_names=True)
+    
+    # checkpointer で保存した中で最もベストだった重みファイルを読み込み、 model を再定義
+    model = None
+    model = load_model(model_filepath)
+    # （後々使うかもしれないので）学習済の重み単体でも保存
+    weights_filepath = parent_dirpath + 'weights.hdf5'
+    model.save_weights(weights_filepath)
+
+    # 一番よかったvalidation accuracyの書き出し
+    val_result_filepath = parent_dirpath + 'ValidationResult.txt'
+    score = model.evaluate(val_img_arrays, val_labels, verbose=0)
+    with open(val_result_filepath, 'w') as f:
+      f.writelines('Best validation loss: ' + str(score[0]) + '\n')
+      f.writelines('Best validation accuracy: ' + str(score[1]) + '\n')
   
   '''
   # 中間層の出力の可視化
@@ -239,6 +243,11 @@ def main():
   classes = model.predict(test_img_arrays, batch_size=64, verbose=1)
   
   # テスト画像およびその識別結果のグラフを表示&保存
+  x = np.array([])
+  for j in range(class_num):
+    x = np.append(x, j) 
+  labels = ['sans-serif', 'serif']
+
   for i in range(0, len(classes)): 
     fig = plt.figure(figsize=(4,3))
     
@@ -247,10 +256,10 @@ def main():
     ax1.imshow(img, cmap='Greys_r')
 
     ax2 = fig.add_subplot(1, 2, 2)
-    x = np.array([0, 1])
     y = classes[i]
-    labels = ['sans-serif', 'serif']
     ax2.bar(left=x, height=y, tick_label=labels, align='center', width=0.5)
+    #ax2.bar(left=x, height=y, align='center', width=0.8)
+    #ax2.set_xlim(0, class_num - 1)
     ax2.set_ylim(0.0, 1.0)
     ax2.set_ylabel('probability')
     ax2.grid(True)
