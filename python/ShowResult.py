d65cb314b5569149e47341b3b9ca4013f01f1597
Ubuntu <shinahara@human.ait.kyushu-u.ac.jp>
2017-06-24 11:43:24 +0900

    バグ(？)改善

--- python/ShowResult.py
+++ python/ShowResult.py
@@ -1,28 +1,27 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import numpy as np
 import os
-import matplotlib.pyplot as plt
 import pickle
 from argparse import ArgumentParser
 from keras.preprocessing import image
 from keras.models import load_model
 from keras.utils import np_utils
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def showAndSaveLossGraph(output_dirpath):
-  history_filepath = output_dirpath + 'history.pickle'
+  history_filepath = mergeFilepaths(output_dirpath, 'history.pickle')
   with open(history_filepath, mode='rb') as f:
     history = pickle.load(f)
   
   # y軸の範囲限定版
-  img_filepath1 = output_dirpath + 'history.png'
+  img_filepath1 = mergeFilepaths(output_dirpath, 'history.png')
   fig1 = plt.figure()
   ax1 = fig1.add_subplot(2, 1, 1)
   ax1.plot(history['loss'], 'o-', label='train-loss')
-  #ax1.plot(history['val_loss'], 'o-', label='val-loss')
+  ax1.plot(history['val_loss'], 'o-', label='val-loss')
   ax1.plot(history['acc'], 'o-', label='train-accuracy')
-  #ax1.plot(history['val_acc'], 'o-', label='val-accuracy')
+  ax1.plot(history['val_acc'], 'o-', label='val-accuracy')
   ax1.set_title('train history')
   ax1.set_xlabel('epoch')
   ax1.set_ylabel('loss')
@@ -33,13 +32,13 @@ def showAndSaveLossGraph(output_dirpath):
   plt.close()
   
   # y軸の範囲限定しない版
-  img_filepath2 = output_dirpath + "history_NoEdit.png"
+  img_filepath2 = mergeFilepaths(output_dirpath, 'history_NoEdit.png')
   fig2 = plt.figure()
   ax2 = fig2.add_subplot(2, 1, 1)
   ax2.plot(history['loss'], 'o-', label='loss')
-  #ax2.plot(history['val_loss'], 'o-', label='val-loss')
+  ax2.plot(history['val_loss'], 'o-', label='val-loss')
   ax2.plot(history['acc'], 'o-', label='accuracy')
-  #ax2.plot(history['val_acc'], 'o-', label='val-accuracy')
+  ax2.plot(history['val_acc'], 'o-', label='val-accuracy')
   ax2.set_title('train history')
   ax2.set_xlabel('epoch')
   ax2.set_ylabel('loss')
@@ -47,154 +46,209 @@ def showAndSaveLossGraph(output_dirpath):
   fig2.savefig(img_filepath2)
   plt.close()
 
-  val_result_filepath = output_dirpath + 'ValidationResult.txt'
-
-def storeTestImages(dirpath):
-  print('\n[Loading images from %s]' % dirpath)
-  img_arrays, filenames = [], []
-  counter = 0
-  for filename in os.listdir(dirpath):
-    if os.path.splitext(filename)[1] != '.png':
-      continue
-    filenames.append(filename)
+def mergeFilepaths(dirpath, filename):
+  if dirpath[-1] == '/':
     filepath = dirpath + filename
-    img = image.load_img(filepath, grayscale=True, target_size=(100,100))
-    img_array = image.img_to_array(img) / 255
-    img_arrays.append(img_array)
-    counter += 1
+  else:
+    filepath = dirpath + '/' + filename
+  return filepath
 
+def countLineNum(txt_filepath):
+  line_num = 0
+  with open(txt_filepath) as f:
+    line = f.readline()
+    while line:
+      line_num += 1
+      line = f.readline()
+  return line_num
+
+def isImage(filepath):
+  img_exts = ['.png', '.jpg']
+  ext = os.path.splitext(filepath)[1]
+  for i in range(len(img_exts)):
+    if ext == img_exts[i]:
+      return True
+  return False
+
+def storeImagesAndLabels(txt_filepath, class_num):
+  print('\n[Loading "{}"]'.format(txt_filepath))
+  total = countLineNum(txt_filepath)
+  print('.. total: {} files'.format(total))
+
+  img_arrays, labels = [], []
+  with open(txt_filepath) as f:
+    line = f.readline()
+    while line:
+      filepath = line.split(' ')[0]
+      if isImage(filepath):
+        img = image.load_img(filepath, grayscale=True)
+        array = image.img_to_array(img) / 255.
+      elif os.path.splitext(filepath)[1] == '.npy':
+        array = np.load(filepath) / 255.
+      img_arrays.append(array)
+      label = line.split(' ')[1]
+      label = label.split('\n')[0]
+      labels.append(label)
+      line = f.readline()
+  img_arrays = np.array(img_arrays)
+  labels = np_utils.to_categorical(labels, class_num)
+  return img_arrays, labels
+
+
+def storeImagesAndFilenamesFromDirpath(path):
+  print('\n[Loading images from "{}"]'.format(path))  
+  img_arrays, filenames = [], []
+  if os.path.isdir(path):
+    for filename in os.listdir(path):
+      if isImage(filename):
+        filenames.append(filename)
+        img = image.load_img(path + filename, grayscale=True, target_size=(100, 100))
+        img_array = image.img_to_array(img) / 255.
+        img_arrays.append(img_array)
+  elif os.path.isfile(path):
+    filenames.append(os.path.basename(path))
+    img = image.load_img(path, grayscale=True, target_size=(100, 100))
+    img_array = image.img_to_array(img) / 255.
+    img_arrays.append(img_array)
   img_arrays = np.array(img_arrays)
   return img_arrays, filenames
 
+
 def storeFontIndex(txt_filepath):
-  dict = {}
+  index_font_dict = {}
   with open(txt_filepath) as f:
     line = f.readline()
     while line:
       index = int(line.split(' ')[0])
       font = line.split(' ')[1]
       font = font.split('\n')[0]
-      dict[index] = font
+      index_font_dict[index] = font
       line = f.readline()
-  return dict
+  return index_font_dict
+
 
 def getFontImage(target_font, dirpath):
   for filename in os.listdir(dirpath):
     stem, ext = os.path.splitext(filename)
-    if ext == '.png':
-      if '_' in stem:
-        font = stem.split('_')[1]
-      else:
-        font = stem      
-      
+    if isImage(filename):
+      font = stem.split('_')[1] if ('_' in stem) else stem
       if font == target_font:
-        _img = image.load_img(dirpath+filename, grayscale=True, target_size=(100, 100))
-        return _img
-  
+        img = image.load_img(mergeFilepaths(dirpath, filename), grayscale=True)
+        return img
 
 
 def main():
   usage = 'Usage: python {} TARGET_DIRPATH CLASS_NUM [--help]'.format(__file__)
   parser = ArgumentParser(usage)
-  parser.add_argument('target_dirpath', type=str, 
-                      help='The path of target directory (must end "/")')
+  parser.add_argument('target_dirpath', type=str, help='path of target directory')
   parser.add_argument('font_list_filepath', type=str, 
-                      help='List of selected fonts and these indices')  
-  parser.add_argument('class_num', type=int, help='Number of classes')
-  parser.add_argument('alphabet', type=str, help='Target alphabet(e.g. "capA", "smallA") or "all"')  
+                      help='list of selected fonts and these indices')  
+  parser.add_argument('class_num', type=int, help='number of classes')
+  parser.add_argument('-a', '--alphabet', type=str, default=None,
+                      help='target alphabet; e.g. "capA", "smallA" (default: None)')
+  # parser.add_argument('-f', '--font', type=str, default=None, help='target font (default: None)') 
   args = parser.parse_args()
 
-  model_filepath = args.target_dirpath + 'model.hdf5'
+  model_filepath = mergeFilepaths(args.target_dirpath, 'model.hdf5')
   model = load_model(model_filepath)
-  
+
   # lossのグラフ表示&保存
-  showAndSaveLossGraph(output_dirpath=args.target_dirpath)
+  if not os.path.isfile(mergeFilepaths(args.target_dirpath, 'history.png')):
+    showAndSaveLossGraph(output_dirpath=args.target_dirpath)
 
   # Validation accuracyの書き出し
-  '''
-  val_filepath = args.target_dirpath + 'validation.txt'
-  val_features, val_labels = [], []
-  with open(val_filepath) as f:
-    line = f.readline()
-    while line:
-      array = np.load(line.split(' ')[0])
-      array /= 255.
-      val_features.append(array)
-      label = line.split(' ')[1]
-      label = label.split('\n')[0]
-      val_labels.append(label)
-      line = f.readline()
-  val_features = np.array(val_features)
-  val_labels = np_utils.to_categorical(val_labels, args.class_num)
+  result_filepath = mergeFilepaths(args.target_dirpath, 'ValidationResult.txt')
+  if not os.path.isfile(result_filepath):
+    val_filepath = mergeFilepaths(args.target_dirpath, 'validation.txt')
+    val_img_arrays, val_labels = storeImagesAndLabels(val_filepath, args.class_num)
+    score = model.evaluate(val_img_arrays, val_labels)
+    with open(result_filepath, 'w') as f:
+      f.writelines('Best validation loss: ' + str('%03.3f' % score[0]) + '\n')
+      f.writelines('Best validation accuracy: ' + str('%03.3f' % score[1]))
+  
+  # key:フォントのindex、 value:フォント
+  index_font_dict = storeFontIndex(args.font_list_filepath)
 
-  score = model.evaluate(val_features, val_labels)
-  val_result_filepath = args.target_dirpath + 'ValidationResult.txt'
-  with open(val_result_filepath, 'w') as f:
-    f.writelines('Best validation loss: ' + str('%03.3f' % score[0]) + '\n')
-    f.writelines('Best validation accuracy: ' + str('%03.3f' % score[1]))
-  '''
-
-  # テスト画像およびその識別結果のグラフを表示&保存
-  def outputImages(target_font=None):
-    IMG_DIRPATH = '/media/snhryt/Data/Research_Master/Syn_AlphabetImages/'
-    if args.alphabet == 'all':
-      test_img_dirpath = IMG_DIRPATH + target_font + '/'
-      output_dirpath = args.target_dirpath + target_font + '/'
-      font_img_dirpath = IMG_DIRPATH + 'FontMontage/all/'
-    else:
-      output_dirpath = args.target_dirpath + 'OutputImages/'      
+  # テスト画像およびその識別結果を描画&保存
+  def drawRecogResultImages(font=None):
+    PARENT_DIRPATH = '/media/snhryt/Data/Research_Master/Syn_AlphabetImages/'
+    output_dirpath = mergeFilepaths(args.target_dirpath, 'OutputImages')
+    if font and args.alphabet:
       if 'cap' in args.alphabet:
-        test_img_dirpath = IMG_DIRPATH + 'capital/' + args.alphabet[-1] + '/'
+        test_img_dirpath = PARENT_DIRPATH + 'capital/' + args.alphabet[-1]
       elif 'small' in args.alphabet:
-        test_img_dirpath = IMG_DIRPATH + 'small/' + args.alphabet[-1] + '/'
-      font_img_dirpath = test_img_dirpath      
-    test_img_arrays, filenames = storeTestImages(test_img_dirpath)
-    results = model.predict(test_img_arrays, batch_size=64, verbose=1)
+        test_img_dirpath = PARENT_DIRPATH + 'small/' + args.alphabet[-1]        
+      test_img_filename = args.alphabet + '_' + font + '.png'
+      test_img_filepath = mergeFilepaths(test_img_dirpath, test_img_filename)
+      font_img_dirpath = test_img_dirpath
+      test_img_arrays, filenames = storeImagesAndFilenamesFromDirpath(path=test_img_filepath)      
+    else:
+      if font:
+        test_img_dirpath = PARENT_DIRPATH + 'font/' + font
+        output_dirpath = mergeFilepaths(output_dirpath, font)
+        font_img_dirpath = PARENT_DIRPATH + 'FontMontage/all'
+      elif args.alphabet:
+        if 'cap' in args.alphabet:
+          test_img_dirpath = PARENT_DIRPATH + 'capital/' + args.alphabet[-1]
+        elif 'small' in args.alphabet:
+          test_img_dirpath = PARENT_DIRPATH + 'small/' + args.alphabet[-1]
+        font_img_dirpath = test_img_dirpath 
+      test_img_arrays, filenames = storeImagesAndFilenamesFromDirpath(path=test_img_dirpath)
+    results = model.predict(test_img_arrays, batch_size=256, verbose=1)
     
     if not os.path.isdir(output_dirpath):
       os.mkdir(output_dirpath)
-    
-    # key:フォントのindex、 value:フォント
-    index_font_dict = storeFontIndex(args.font_list_filepath)
-
-    print('\n[Saving result images to %s]' % output_dirpath)
-    TOP_NUM = 3
+  
+    TOP_N = 3
+    if args.class_num < TOP_N:
+      TOP_N = args.class_num
+    print('\n[Saving recognition result images to {}]'.format(output_dirpath))
     for i in range(len(results)):
-      test_img = image.array_to_img(test_img_arrays[i], scale=True)
       font_prob_dict = {}
       for j in range(args.class_num):
         font_prob_dict[index_font_dict[j]] = results[i][j]
+      # probability が高い順にソート
       font_prob_dict = sorted(font_prob_dict.items(), key=lambda x:x[1], reverse=True)
-      
-      fig, axes = plt.subplots(nrows=2,ncols=TOP_NUM, figsize=(8,6))
-      axes[0, 0].imshow(test_img, cmap='Greys_r')
-      for j in range(1, TOP_NUM):
-        axes[0, j].axis('off')    
-      for j in range(TOP_NUM):
-        if j >= args.class_num:
-          axes[1, j].axis('off')
+
+      ncols = 1
+      # もし TOP_N にprobability=0.0のものが含まれていれば描画しない
+      for j in range(TOP_N):
+        if font_prob_dict[j][1] * 1000 <= 1:
           break
-        font = font_prob_dict[j][0]
-        prob = font_prob_dict[j][1]
-        if prob < 0.1:
-          axes[1, j].axis('off')          
-        else:
-          font_img = getFontImage(font, font_img_dirpath)
-          axes[1, j].imshow(font_img, cmap='Greys_r')
-          title = 'top' + str(j + 1) + ': ' + str('%03.1f' % (prob*100)) + '%'
-          axes[1, j].set_title(title, fontsize=16)
+        ncols += 1
+      fig, axes = plt.subplots(nrows=1, ncols=ncols)
+
+      test_img = image.array_to_img(test_img_arrays[i], scale=True)
+      axes[0].imshow(test_img, cmap='Greys_r')
+      axes[0].set_title('input image', fontsize=16)
+
+      for j in range(1, ncols):
+        font = font_prob_dict[j - 1][0]
+        str_prob = '{0:3.1f}'.format(font_prob_dict[j - 1][1] * 100)
+        font_img = getFontImage(font, font_img_dirpath)
+        axes[j].imshow(font_img, cmap='Greys_r')
+        title = 'prob=' + str_prob + '[%]'
+        axes[j].set_title(title, fontsize=14)
 
       fig.tight_layout() # タイトルとラベルが被らないようにする
       # plt.pause(0.7)
       plt.close()
-      output_img_filepath = output_dirpath + filenames[i]
+      output_img_filepath = mergeFilepaths(output_dirpath, filenames[i])
       fig.savefig(output_img_filepath)
       if (i + 1) % 10 == 0:
-        print('.. %d/%d result images are saved' % (i + 1, len(results)))
+        print('.. {0}/{1} result images are saved'.format(i + 1, len(results)))
     
-  outputImages()
-    
-      
+  drawRecogResultImages(font='A750-Sans-Medium-Regular')
+  drawRecogResultImages(font='A850-Roman-Regular')
+  drawRecogResultImages(font='Accidental-Presidency')
+  drawRecogResultImages(font='Action-Man')
+  drawRecogResultImages(font='AdelonSerial-Xbold-Regular')
+  drawRecogResultImages(font='Aesop-Regular')
+  drawRecogResultImages(font='Airmole')
+  drawRecogResultImages(font='AkazanLt-Regular')
+  drawRecogResultImages(font='Amplitude-BRK')
+  drawRecogResultImages(font='Avondale-SC')
+  
 
 if __name__ == "__main__":
   main() 
--- python/TrainCaffenet.py
+++ python/TrainCaffenet.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import numpy as np
 import pickle
 from argparse import ArgumentParser
-from keras.models import Sequential, load_model, model_from_json
+from keras.models import Sequential
+from keras.preprocessing import image
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.utils import np_utils, plot_model
+import numpy as np
 
+CHANNEL_NUM, WIDTH, HEIGHT = 1, 100, 100
+
+def makeFullFilepath(dirpath, filename):
+  if dirpath[-1] == '/':
+    filepath = dirpath + filename
+  else:
+    filepath = dirpath + '/' + filename
+  return filepath
 
 def buildCaffenet(class_num, channel_num=3, img_width=224, img_height=224):
   from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
@@ -19,9 +28,11 @@ def buildCaffenet(class_num, channel_num=3, img_width=224, img_height=224):
 
   model = Sequential()
   # Conv1
-  model.add(Convolution2D(nb_filter=96, nb_row=5, nb_col=5, border_mode='valid', 
-                          input_shape=(img_width, img_height, channel_num), subsample=(2, 2), 
-                          W_regularizer=l2(weight_decay), name='conv1'))
+  model.add(
+    Convolution2D(filters=96, kernel_size=(5,5), border_mode='valid', 
+                  input_shape=(img_width, img_height, channel_num), 
+                  subsample=(2, 2), W_regularizer=l2(weight_decay), name='conv1')
+  )
   model.add(Activation('relu'))
   model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
   model.add(BatchNormalization())
@@ -47,11 +58,11 @@ def buildCaffenet(class_num, channel_num=3, img_width=224, img_height=224):
   
   model.add(Flatten())
   # Fc6
-  model.add(Dense(4096,  W_regularizer=l2(weight_decay), name='fc6'))
+  model.add(Dense(4096, W_regularizer=l2(weight_decay), name='fc6'))
   model.add(Activation('relu'))
   model.add(Dropout(0.5))
   # Fc7
-  model.add(Dense(4096,  W_regularizer=l2(weight_decay), name='fc7'))
+  model.add(Dense(4096, W_regularizer=l2(weight_decay), name='fc7'))
   model.add(Activation('relu'))
   model.add(Dropout(0.5))
   # Fc8
@@ -59,7 +70,6 @@ def buildCaffenet(class_num, channel_num=3, img_width=224, img_height=224):
   model.add(Activation('softmax'))
 
   model.summary()
-  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
   return model
 
 
@@ -73,98 +83,125 @@ def countLineNum(txt_filepath):
   return line_num
 
 
-def storeNpyFilepathsAndLabels(txt_filepath, class_num):
-  print('\n[Loading %s]' % txt_filepath)
+def storeImageFilepathsAndLabels(txt_filepath, class_num):
+  print('\n[Loading "{}"]'.format(txt_filepath))
   total = countLineNum(txt_filepath)
-  print('.. total: %d' % total)
-  
-  npy_filepaths, labels = [], []
+  print('.. total: {} files'.format(total))
+  filepaths, labels = [], []
   with open(txt_filepath) as f:
     line = f.readline()
     while line:
-      npy_filepath = line.split(' ')[0]
+      filepath = line.split(' ')[0]
+      filepaths.append(filepath)
       label = line.split(' ')[1]
       label = label.split('\n')[0]
-      npy_filepaths.append(npy_filepath)
       labels.append(label)
       line = f.readline()
-  return npy_filepaths, labels
+  labels = np_utils.to_categorical(labels, class_num)
+  return filepaths, labels
+
+
+def generateArrays(filepaths, labels, batch_size, class_num):
+  batch_img_arrays = np.zeros((batch_size, WIDTH, HEIGHT, CHANNEL_NUM))
+  batch_labels = np.zeros((batch_size, class_num))
+  img_ext = os.path.splitext(filepaths[0])[1]
+  counter = 0
+  while counter < len(filepaths):
+    for i in range(batch_size):
+      if img_ext == '.png' or img_ext == '.jpg':
+        img = image.load_img(filepaths[counter], grayscale=True, target_size=(WIDTH, HEIGHT))
+        array = image.img_to_array(img) / 255.
+      elif img_ext == '.npy':
+        array = np.load(filepaths[counter]) / 255.
+      batch_img_arrays[i] = array.reshape(1, WIDTH, HEIGHT, CHANNEL_NUM)
+      batch_labels[i] = labels[counter]
+      counter += 1
+    yield (batch_img_arrays, batch_labels)
+
+
+def loadImages(filepaths):
+  img_ext = os.path.splitext(filepaths[0])[1]
+  print ('\n[Loading {0} {1} images]'.format(len(filepaths), img_ext))
+  img_arrays = np.zeros((len(filepaths), WIDTH, HEIGHT, CHANNEL_NUM))
+  for (i, path) in enumerate(filepaths):
+    if img_ext == '.png' or img_ext == '.jpg':
+      img = image.load_img(path, grayscale=True, target_size=(WIDTH, HEIGHT))
+      array = image.img_to_array(img) / 255.
+    elif img_ext == '.npy':
+      array = np.load(path) / 255.
+    img_arrays[i] = array.reshape(1, WIDTH, HEIGHT, CHANNEL_NUM)
+    if (i + 1) % 1000 == 0:
+      print ('.. {} images are loaded'.format(i + 1))
+  return img_arrays
 
 
 def main():
-  usage = 'Usage: python {} TARGET_DIRPATH CLASS_NUM [--batch_size] [--epochs] [--help]'.format(__file__)
+  # コマンドラインから引数を与える
+  usage = ('Usage: python {} TARGET_DIRPATH CLASS_NUM [--generator_training]'.format(__file__)
+           + '[--batch_size] [--epochs] [--help]')
   parser = ArgumentParser(usage)
-  parser.add_argument('target_dirpath', type=str, 
-                      help='The path of target directory (must end "/")')
-  parser.add_argument('class_num', type=int, help='The number of classes')
-  parser.add_argument('-b', '--batch_size', type=int, default=256, help='Batch size')
-  parser.add_argument('-e', '--epochs', type=int, default=50, help='The number of training epoch')  
+  parser.add_argument('target_dirpath', type=str, help='path of target directory')
+  parser.add_argument('class_num', type=int, help='number of classes')
+  parser.add_argument('-g', '--generator_training', action='store_true', default=False, 
+                      help='mini-batch training using generator (default: False)')  
+  parser.add_argument('-b', '--batch_size', type=int, default=256, 
+                      help='batch size (default: 256)')
+  parser.add_argument('-e', '--epochs', type=int, default=50, 
+                      help='number of training epoch (default: 50)')  
   args = parser.parse_args()
 
-  channel_num, img_width, img_height = 1, 100, 100
-  model_filepath = args.target_dirpath + 'model.hdf5'
-  model = buildCaffenet(class_num=args.class_num, channel_num=channel_num, img_width=img_width,
-                        img_height=img_height)
+  # caffenetの構築およびコンパイル
+  model_filepath = makeFullFilepath(args.target_dirpath, 'model.hdf5')
+  model = buildCaffenet(class_num=args.class_num, channel_num=CHANNEL_NUM, img_width=WIDTH,
+                        img_height=HEIGHT)
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
   # モデル構造の書き出し
-  png_filepath = args.target_dirpath + 'ModelStructure.png'
+  png_filepath = makeFullFilepath(args.target_dirpath, 'ModelStructure.png')
   plot_model(model, to_file=png_filepath, show_shapes=True, show_layer_names=True)
-  
-  train_txt_filepath = args.target_dirpath + 'train.txt'
-  train_filepaths, train_labels = storeNpyFilepathsAndLabels(txt_filepath=train_txt_filepath, 
-                                                              class_num=args.class_num)
-  '''
-  val_txt_filepath = args.target_dirpath + 'validation.txt'
-  val_filepaths, val_labels = storeNpyFilepathsAndLabels(txt_filepath=val_txt_filepath, 
-                                                          class_num=args.class_num)
-  
+
+  # train, validation に使うデータのファイルパスおよびそのラベルを格納
+  train_txt_filepath = makeFullFilepath(args.target_dirpath, 'train.txt')
+  train_filepaths, train_labels = storeImageFilepathsAndLabels(txt_filepath=train_txt_filepath, 
+                                                               class_num=args.class_num)
+  val_txt_filepath = makeFullFilepath(args.target_dirpath, 'validation.txt')
+  val_filepaths, val_labels = storeImageFilepathsAndLabels(txt_filepath=val_txt_filepath, 
+                                                           class_num=args.class_num)
+
   # callbacksの設定
-  checkpointer = ModelCheckpoint(model_filepath, monitor='val_loss', verbose=1, 
+  checkpointer = ModelCheckpoint(filepath=model_filepath, monitor='loss', verbose=1, 
                                   save_best_only=True, save_weights_only=False)
   # patientce： 何回連続で損失の最小値が更新されなかったらループを止めるか
-  early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
-  tensor_board = TensorBoard(log_dir=args.target_dirpath+'graph', histogram_freq=0, write_graph=True)
-  
-  # generator を生成して batch_size 枚の画像ごとに学習を行う
-  def generateArrays(filepaths, labels):
-    batch_features = np.zeros((args.batch_size, img_width, img_height, channel_num))
-    batch_labels = np.zeros((args.batch_size, args.class_num))
-    for i in range(len(filepaths)):
-      for j in range(args.batch_size):
-        array = np.load(filepaths[i])
-        array /= 255.
-        batch_features[j] = array.reshape(1, img_width, img_height, channel_num)        
-        batch_labels[j] = np_utils.to_categorical(labels[i], args.class_num)
-      yield (batch_features, batch_labels)
+  early_stopping = EarlyStopping(monitor='loss', patience=10, verbose=1)
+  tensor_board = TensorBoard(log_dir=args.target_dirpath+'graph', histogram_freq=0, 
+                             write_graph=True)
 
-  steps_per_epoch = int(len(train_filepaths) / args.batch_size)
-  validation_steps = int(len(val_filepaths) / args.batch_size)
-  history = model.fit_generator(generateArrays(filepaths=train_filepaths, labels=train_labels),
-                                steps_per_epoch=steps_per_epoch, epochs=args.epochs, 
-                                validation_data=generateArrays(filepaths=val_filepaths,  
-                                                                labels=val_labels),
-                                validation_steps=validation_steps,
-                                callbacks=[checkpointer, early_stopping, tensor_board])
-  '''
-
-  checkpointer = ModelCheckpoint(model_filepath, monitor='acc', verbose=1, 
-                                  save_best_only=True, save_weights_only=False)
-  early_stopping = EarlyStopping(monitor='acc', patience=10, verbose=1)
-  tensor_board = TensorBoard(log_dir=args.target_dirpath+'graph', histogram_freq=0, write_graph=True)
-  def loadNpys(filepaths):
-    features = np.zeros((len(filepaths), img_width, img_height, channel_num))
-    for i in range(len(filepaths)):
-      array = np.load(filepaths[i])
-      array /= 255.
-      features[i] = array
-    return features
-
-  train_features = loadNpys(train_filepaths)
-  train_labels = np_utils.to_categorical(train_labels, args.class_num)
-  history = model.fit(train_features, train_labels, batch_size=args.batch_size, epochs=args.epochs,
-                      callbacks=[checkpointer, early_stopping, tensor_board])
+  # generator を生成して batch_size 枚の画像(.npy)ごとに学習を行う
+  if args.generator_training:
+    steps_per_epoch = int(len(train_filepaths) / args.batch_size)
+    validation_steps = int(len(val_filepaths) / args.batch_size)
+    history = model.fit_generator(
+      generateArrays(filepaths=train_filepaths, labels=train_labels, batch_size=args.batch_size, 
+                     class_num=args.class_num),
+      steps_per_epoch=steps_per_epoch,
+      epochs=args.epochs, 
+      validation_data=generateArrays(filepaths=val_filepaths, labels=val_labels, 
+                                     batch_size=args.batch_size, class_num=args.class_num),
+      validation_steps=validation_steps,
+      callbacks=[checkpointer, early_stopping]
+    )
+  else:
+    # train, validationの画像をすべて読み込んで学習を行う
+    train_img_arrays = loadImages(train_filepaths)
+    val_img_arrays = loadImages(val_filepaths)
+    history = model.fit(
+      train_img_arrays, train_labels, batch_size=args.batch_size, epochs=args.epochs, 
+      validation_data=(val_img_arrays,val_labels),
+      callbacks=[checkpointer, early_stopping, tensor_board]
+    )
   
   # 学習履歴の保存
-  history_filepath = args.target_dirpath + 'history.pickle'
+  history_filepath = makeFullFilepath(args.target_dirpath, 'history.pickle')
   with open(history_filepath, mode='wb') as f:
     pickle.dump(history.history, f)
 
